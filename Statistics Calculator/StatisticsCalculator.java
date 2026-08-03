5c5379e6360874b6a4726df246f190d1b6638cb0
Abdulkadir110 <sheuabdulquadir17@gmail.com>
2026-08-03 15:25:22 +0100

    new works

---
+++ Statistics Calculator/StatisticsCalculator.java
@@ -0,0 +1,150 @@
+public class StatisticsCalculator{
+	public static int calculateRangeOf(int[] numbers){
+	
+		return findLargestNumberIn(numbers) - findSmallestNumberIn(numbers);
+
+	}
+
+
+	private static int findLargestNumberIn(int[] numbers){
+		int largest = numbers[0];
+		for(int index = 0; index < numbers.length; index++){
+			if(numbers[index] > largest)
+				largest = numbers[index];
+		}
+		return largest;
+	}
+
+	private static int findSmallestNumberIn(int[] numbers){
+		int smallest = numbers[0];
+		for(int index = 0; index < numbers.length; index++){
+			if(numbers[index] < smallest)
+				smallest = numbers[index];
+		}
+		return smallest;
+	}
+    
+
+    public static double calculateMeanOf(int[] numbers) {
+        int sum = 0; 
+        double meanOfTheNumbers = 0;
+       
+        for (int index = 0; index < numbers.length; index++) {
+                sum += numbers[index];
+        }
+            return sum / numbers.length;
+    }
+    /////////////////////////////////////////////////--Median--//////////////////////////////////////////
+    public static int calculateMedianOf(int[] numbers) {
+        int median = 0;
+        int medianIndex = 0;
+        int[] descendingArrayOfNumbers = descendingArray(numbers);
+        int lengthOfDescendingArrayOfNumbers = descendingArrayOfNumbers.length;
+        if(lengthOfDescendingArrayOfNumbers % 2 == 0) {
+             int firstNumberInTheMiddleIndex = (lengthOfDescendingArrayOfNumbers / 2 ) - 1;
+             int SecondNumberInTheMiddleIndex = (lengthOfDescendingArrayOfNumbers / 2 );
+
+            medianIndex = (firstNumberInTheMiddleIndex + SecondNumberInTheMiddleIndex) / 2;
+            return numbers[medianIndex];
+        }
+        else {
+                medianIndex =  numbers.length / 2 ;               
+                return numbers[medianIndex];
+        }
+    }
+    
+    private static int[] descendingArray(int[] numbers) {
+
+        for (int index = 0; index < numbers.length; index++) {
+
+                for(int newIndex = 0; newIndex < numbers.length; newIndex++) {
+
+                    if(numbers[newIndex] < numbers[index]) {
+                        int temp = numbers[newIndex];
+                        numbers[newIndex] = numbers[index];
+                        numbers[index] = temp;
+                        
+                    }
+                        
+                }
+
+        }
+
+      return numbers;
+    }
+    //////////////////////////////////////////--Mode--////////////////////////////////////////////////////
+    
+    public static int calculateModeOf(int[] numbers) {
+        int modeCounter = 0;
+        int mode = 0;
+        int temp = 0;
+        for(int index = 0; index < numbers.length; index++) {
+            modeCounter = 0;
+               for(int counterIndex = 0; counterIndex < numbers.length; counterIndex++) {
+                    if(numbers[index] == numbers[counterIndex]) {
+                        modeCounter++;
+                        mode = numbers[index];
+                        
+                    }
+                }
+                    temp = modeCounter;
+             if (modeCounter > temp) {
+                   mode = numbers[index];
+             }          
+        }
+        return mode;
+          
+    }
+
+     
+    ///////////////////////////////////////////---Deviation--///////////////////////////////////////////////
+    
+    public static double[] calculateDeviationOf(int [] numbers) {
+        double sum = 0; 
+        double differenceBetweenEachNumberAndMean = 0;
+        double[] deviation = new double [numbers.length];
+        for (int index = 0; index < numbers.length; index++) {
+                 differenceBetweenEachNumberAndMean = numbers[index] - calculateMeanOf(numbers);
+                 deviation[index] += differenceBetweenEachNumberAndMean;
+        }
+        return deviation;
+    }   
+    
+     public static double calculateMeanDeviationOf(int [] numbers) {
+        double deviation = 0;
+        double sum = 0;
+        double[] deviationCollection = calculateDeviationOf(numbers);
+        for (int index = 0; index < numbers.length; index++) {
+             sum += deviationCollection[index]; 
+        }
+            deviation = sum / numbers.length;
+    
+        return deviation;
+    }
+    
+    public static double calculateVarianceOf(int [] numbers) {
+        double variance = 0;
+        double squareDeviation = 0;
+        double sum = 0; 
+        double differenceBetweenEachNumberAndMean = 0;
+
+        for (int index = 0; index < numbers.length; index++) {
+                 differenceBetweenEachNumberAndMean = numbers[index] - calculateMeanOf(numbers);
+                 sum += differenceBetweenEachNumberAndMean;
+        }
+        squareDeviation = sum * sum;
+        variance = squareDeviation / numbers.length - 1;
+    
+        return variance;
+    } 
+
+    public static double calculateStandardDeviationOf(int [] numbers) {
+        double standardDeviation = calculateVarianceOf(numbers) / numbers.length;
+        return standardDeviation;
+    }
+    public static double calculateCoefficientOfVarianceOf(int[] numbers) {
+        double coefficientOfVariance = (calculateStandardDeviationOf(numbers) / calculateMeanOf(numbers)) * 100;
+        return coefficientOfVariance;
+    }   
+}
+
---
+++ Statistics Calculator/StatisticsCalculatorTest.java
@@ -0,0 +1,155 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class StatisticsCalculatorTest{
+	
+	@Test
+	public void testThatIHaveAnArrayOfNumbersRange(){
+
+	//Given
+	int [] numbers = {3, 5, 7, 1, 25, 11, 0};
+
+	//When
+	int expectedRange = StatisticsCalculator.calculateRangeOf(numbers);
+
+	int actualRange = 25;
+
+	//Check
+	assertEquals(expectedRange, actualRange);
+
+
+
+	}
+    
+    @Test
+    public void testThatIHaveAnArrayOfNumbersMean() {
+    
+    //Given
+	int [] numbers = {3, 5, 7, 1, 25, 11, 0};
+
+	//When
+	double expectedMean = StatisticsCalculator.calculateMeanOf(numbers);
+
+    double actualMean = 7.0;
+
+
+    //Check
+    assertEquals(expectedMean, actualMean);
+    }
+	
+    @Test
+    public void testThatIHaveAnArrayOfNumbersMedian() {
+    
+    //Given
+	int [] numbers = {3, 5, 7, 1, 25, 11, 0};
+
+	//When
+	int expectedMedian = StatisticsCalculator.calculateMedianOf(numbers);
+
+    int actualMedian = 5;
+
+
+    //Check
+    assertEquals(expectedMedian, actualMedian);
+    }
+
+    
+    @Test
+    public void testThatIHaveAnArrayOfNumbersMode() {
+    
+    //Given
+	int [] numbers = {3, 5, 2, 1, 2, 11, 0, 2};
+
+	//When
+	int expectedMode = StatisticsCalculator.calculateModeOf(numbers);
+
+    int actualMode = 2;
+
+
+    //Check
+    assertEquals(expectedMode, actualMode);
+    }
+
+    @Test
+    public void testThatIHaveAnArrayOfNumbersDeviation() {
+    
+    //Given
+	int [] numbers = {3, 5, 7, 1, 25, 11, 0};
+
+	//When
+	double[] expectedDeviation = StatisticsCalculator.calculateDeviationOf(numbers);
+
+    double[] actualDeviation = {-4, -2 , 0, -6, 18, 4 ,-7};
+
+
+    //Check
+    assertArrayEquals(expectedDeviation, actualDeviation);
+    }
+    @Test
+    public void testThatIHaveAnArrayOfNumbersMeanDeviation() {
+    
+    //Given
+	int [] numbers = {3, 5, 7, 1, 25, 11, 0};
+
+	//When
+	double expectedMeanDeviation = StatisticsCalculator.calculateMeanDeviationOf(numbers);
+
+    double actualMeanDeviation = 0.42857142857142855;
+
+
+    //Check
+    assertEquals(expectedMeanDeviation, actualMeanDeviation);
+    }
+
+    @Test
+    public void testThatIHaveAnArrayOfNumbersVariance() {
+    
+    //Given
+	int [] numbers = {3, 5, 7, 1, 25, 11, 0};
+
+	//When
+	double expectedVariance = StatisticsCalculator.calculateVarianceOf(numbers);
+
+    double actualvariance = 0.2857142857142858;
+
+
+    //Check
+    assertEquals(expectedVariance, actualvariance);
+    }    
+
+     @Test
+    public void testThatIHaveAnArrayOfNumbersStandardDeviation() {
+    
+    //Given
+	int [] numbers = {3, 5, 7, 1, 25, 11, 0};
+
+	//When
+	double expectedStandardDeviation = StatisticsCalculator.calculateStandardDeviationOf(numbers);
+
+    double actualStandardDeviation = 0.040816326530612256;
+
+
+    //Check
+    assertEquals(expectedStandardDeviation, actualStandardDeviation);
+    }
+    
+    @Test
+    public void testThatIHaveAnArrayOfNumbersCoefficientOfVariance() {
+    
+    //Given
+	int [] numbers = {3, 5, 7, 1, 25, 11, 0};
+
+	//When
+	double expectedCoefficientOfVariance = StatisticsCalculator.calculateCoefficientOfVarianceOf(numbers);
+
+    double actualCoefficientOfVariance = 0.5830903790087465;
+
+
+    //Check
+    assertEquals(expectedCoefficientOfVariance, actualCoefficientOfVariance);
+    }
+
+}
