f70491af773461ac289bbdbb5f0703164598fc02
Abdulkadir110 <sheuabdulquadir17@gmail.com>
2026-09-25 19:05:14 +0100

    bank Account successful<<<<<<<<

--- bankAccount/src/tdd/Account.java
+++ bankAccount/src/tdd/Account.java
@@ -2,15 +2,33 @@ package tdd;
 
 public class Account {
     private double balance;
-    public double getBalance() {
+    private String pin;
+
+    public double getBalance(String pin) {
+        if(!pin.equals(this.pin)) throw new IllegalArgumentException("Incorrect Pin");
         return balance;
     }
     public void deposit(double amount) {
         if(amount > 0)balance += amount;
     }
-    public void withdraw(double amount) {
-        if(amount > 0 && amount <= balance){
+    public void withdraw(double amount, String pin) {
+        if(amount > 0 && amount <= balance && pin.equals(this.pin)) {
             balance -= amount;
         }
+        else if (!pin.equals(this.pin)){
+            throw new IllegalArgumentException("Incorrect Pin");
+        }
     }
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+    public void updatePin(String oldPin, String newPin){
+        if(oldPin.equals(this.pin)){
+            this.pin = newPin;
+        }
+        else{
+            throw new IllegalArgumentException("Incorrect Pin");
+        }
+    }
+
 }
--- bankAccount/test/tdd/AccountTest.java
+++ bankAccount/test/tdd/AccountTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import tdd.Account;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AccountTest {
     private Account myAccount;
@@ -14,31 +15,76 @@ public class AccountTest {
     @Test
     public void testThatIDeposit5kAndTheBalanceIs5k(){
         //Given
-        assertEquals(0.0, myAccount.getBalance());
+        myAccount.setPin("1111");
+        assertEquals(0.0, myAccount.getBalance("1111"));
         //When
         myAccount.deposit(5_000);
         //check
-        assertEquals(5000, myAccount.getBalance());
+        assertEquals(5000, myAccount.getBalance("1111"));
     }
     @Test
     public void testThatIDepositMinus5kAndBalanceRemainsTheSame(){
         //Given
-        assertEquals(0.0, myAccount.getBalance());
+        myAccount.setPin("1111");
+        assertEquals(0.0, myAccount.getBalance("1111"));
         //When
         myAccount.deposit(-5_000);
         //Check
-        assertEquals(0.0, myAccount.getBalance());
+        assertEquals(0.0, myAccount.getBalance("1111"));
     }
     @Test
     public void testThatIWithdraw2kFrom5k_TheBalanceIs3k() {
         //Given
-        assertEquals(0.0, myAccount.getBalance());
+        myAccount.setPin("1111");
+        assertEquals(0.0, myAccount.getBalance("1111"));
         //When
         myAccount.deposit(5_000);
-        assertEquals(5000, myAccount.getBalance());
-        myAccount.withdraw(2000);
-        assertEquals(3000, myAccount.getBalance());
+        assertEquals(5000, myAccount.getBalance("1111"));
+        myAccount.withdraw(2000,"1111");
+        assertEquals(3000, myAccount.getBalance("1111"));
+    }
+    @Test
+    public void testThatICantWithdrawNegativeAmount(){
+        myAccount.setPin("1111");
+        assertEquals(0.0, myAccount.getBalance("1111"));
+        myAccount.deposit(5_000);
+        myAccount.withdraw(-2000,"1111");
+        assertEquals(5000, myAccount.getBalance("1111"));
+    }
+    @Test
+    public void testThatICantWithdrawWithIncorrectPin(){
+        myAccount.setPin("1111");
+        assertEquals(0.0, myAccount.getBalance("1111"));
+        myAccount.deposit(5_000);
+        assertThrows(IllegalArgumentException.class, () -> myAccount.withdraw(2000,"1121"));
+        assertEquals(5000, myAccount.getBalance("1111"));
+    }
+    @Test
+    public void testToCheckBalanceWithIncorrectPin(){
+        myAccount.setPin("1111");
+        assertThrows(IllegalArgumentException.class,() -> myAccount.getBalance("9028"));
+    }
+    @Test
+    public void testThatAccountPinWasUpdated(){
+        myAccount.setPin("1121");
+        assertEquals(0.0, myAccount.getBalance("1121"));
+        myAccount.deposit(5_000);
+        myAccount.withdraw(2000,"1121");
+        assertEquals(3000, myAccount.getBalance("1121"));
+        myAccount.updatePin("1121", "1111");
+        myAccount.withdraw(2000,"1111");
+        assertEquals(1000, myAccount.getBalance("1111"));
+        assertThrows(IllegalArgumentException.class, () -> myAccount.withdraw(500,"1121"));
+        assertEquals(1000, myAccount.getBalance("1111"));
+    }
+    @Test
+    public void testThatAccountPinWasNotUpdated(){
+        myAccount.setPin("1121");
+        assertEquals(0.0, myAccount.getBalance("1121"));
+        myAccount.deposit(5_000);
+        assertThrows(IllegalArgumentException.class, () -> myAccount.updatePin("2345", "1234"));
+        assertThrows(IllegalArgumentException.class, () -> myAccount.withdraw(4000, "1234"));
+        assertEquals(5000, myAccount.getBalance("1121"));
 
     }
-
 }
